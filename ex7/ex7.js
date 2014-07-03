1b3e08a590da28775b3f3bc8d4df6c71ef05fc82
unenglishable <unenglishable@gmail.com>
2014-07-02 14:02:25 -1000

    the accepted solution for ex7

--- ex7/ex7.js
+++ ex7/ex7.js
@@ -2,13 +2,6 @@ var http = require('http');
 
 http.get(process.argv[2], function (response) {
   response.setEncoding('utf8');
-  response.on('error', function (error) {
-    console.log(error);
-  });
-  response.on('data', function (data) {
-    console.log(data);
-  });
-  response.on('end', function (end) {
-    return end;
-  });
+  response.on('error', console.error);
+  response.on('data', console.log);
 });
