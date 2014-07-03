6009bf2500c8e36d7c71949fbff4efb0dc3ea88f
unenglishable <unenglishable@gmail.com>
2014-07-02 14:30:01 -1000

    update cooltricks

--- cooltricks.js
+++ cooltricks.js
@@ -9,3 +9,22 @@ array.filter(function(element) {
   // keep if element matches condition
   return element === condition;
 });
+
+// open a server in tcp
+var server = net.createServer(function callback (socket) {
+  // do some stuff
+  socket.end('some string);
+});
+server.listen(port);
+
+// open a server in http
+var server = http.createServer(function (request, response) {
+  // use writeHead() to qrite a header in http
+  res.writeHead(200, { 'content-type': 'text/plain' })
+
+  // do some stuff
+  // iStream.pipe(oStream);
+  fs.createReadStream(istream).pipe(reponse);
+});
+server.listen(port);
+
