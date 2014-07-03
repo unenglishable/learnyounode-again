81c0ee39c48a207cb73a2c50f378d0abf2caed08
unenglishable <unenglishable@gmail.com>
2014-07-02 14:24:18 -1000

    Accepted solution (writes HEAD)

--- ex11/ex11.js
+++ ex11/ex11.js
@@ -4,6 +4,8 @@ var port = process.argv[2];
 var srvFile = process.argv[3];
 
 var server = http.createServer(function (request, response) {
+  response.writeHead(200, { 'content-type': 'text/plain' });
+
   fs.createReadStream(srvFile).pipe(response);
 });
 
